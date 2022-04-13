9c838603576e28ad046168caf2824c0b80f3c6cd
Eduardo Joly <eduardo.niceinfo@gmail.com>
2022-04-13 12:41:01 -0300

    Lista e peso das peças

---
+++ pecas.js
@@ -0,0 +1,7 @@
+var listadepecas = ["Motor", "Amortecedor", "Freio", "Cambio", "Disco de Freio", "Radiador"]
+
+if (listadepecas.length < 10){
+    console.log("É possível cadastrar mais peças")
+}else{
+    console.log("Não tem mais espaço na lista")
+}    

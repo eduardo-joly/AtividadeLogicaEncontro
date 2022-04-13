e14f50004cb78b47c0301cfcb7e3dc88f39f1b20
Eduardo Joly <eduardo.niceinfo@gmail.com>
2022-04-13 12:50:50 -0300

    Nome das peças

--- pecas.js
+++ pecas.js
@@ -4,4 +4,20 @@ if (listadepecas.length < 10){
     console.log("É possível cadastrar mais peças")
 }else{
     console.log("Não tem mais espaço na lista")
-}    
+}
+
+
+let peso = 50;
+if (peso,100){
+    console.log("A peça deve pesar no mínimo 100g")
+}else{
+    console.log("A peça possui peso adequado")
+}
+
+
+let nomepecas = "Disco de Freio"
+if (nomepecas.length > 3){
+    console.log("Nome de peça está adequado para o cadastro!")
+}else{
+    console.log("Nome deve ter mais de 3 caracteres, digite um nome adequado")
+} 
